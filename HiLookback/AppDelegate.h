b90e9eb56d2e1486a963f4ff93da37b7f799ab64
Misael <misael@qq.com>
2015-08-30 12:31:00 +0800

    Initial Commit

---
+++ HiLookback/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HiLookback
+//
+//  Created by zhimei on 15/8/30.
+//  Copyright © 2015年 yihu inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HiLookback/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HiLookback
+//
+//  Created by zhimei on 15/8/30.
+//  Copyright © 2015年 yihu inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
